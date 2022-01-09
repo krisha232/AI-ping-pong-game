2f684e5dd8d550b89ec7d90687c91838498d63c4
krisha232 <78255450+krisha232@users.noreply.github.com>
2022-01-09 15:50:16 +0530

    Update main.js

--- main.js
+++ main.js
@@ -23,13 +23,20 @@ var ball = {
 
 function setup(){
   var canvas =  createCanvas(700,600);
+  canvas.parent('canvas');
+  video = createCapture(VIDEO);
+  video.hide();  
+  poseNet=ml5.poseNet(video,modelLoaded);
 }
 
+function modelLoaded(){
+  console.log('Model Loaded!');
+}
 
 function draw(){
 
  background(0); 
-
+image(video,0,0,700,600);
  fill("black");
  stroke("black");
  rect(680,0,20,700);
